aac5a873ece9b552980dad3e450e2adab73f4a89
donfortune <donfortunet.df@gmail.com>
2024-02-06 04:13:23 -0800

    updated base class

---
+++ models/base_model.py
@@ -0,0 +1,40 @@
+from uuid import uuid4
+from datetime import datetime
+   
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        if kwargs:  
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            else:
+                self.id = str(uuid4())
+            if 'created_at' in kwargs:
+                self.created_at = datetime.strptime(kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                self.created_at = datetime.now()
+            if 'updated_at' in kwargs:
+                self.updated_at = datetime.strptime(kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+
+
+
+
+
+    def __str__(self):
+        return '[{}] ({}) {}'.format(type(self).__name__,self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = type(self).__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
