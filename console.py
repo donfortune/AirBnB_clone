b4ae62ce51dd04253d3bf74006d044411f076f94
donfortune <donfortunet.df@gmail.com>
2024-02-11 11:12:19 -0800

    added file

---
+++ console.py
@@ -0,0 +1,27 @@
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+
+        return True
+
+    def
+        print("")
+        return True
+
+    def emptyline(self):
+        pass
+
+    def help_quit(self):
+        print("Quit command: Exits the program")
+
+    def help_EOF(self):
+        print("EOF command: Exits the program with Ctrl+D")
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+
