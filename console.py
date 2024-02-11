c82f491f9a850d54c1df570e0a4213b43c91c3d1
donfortune <donfortunet.df@gmail.com>
2024-02-11 11:32:13 -0800

    added file

--- console.py
+++ console.py
@@ -6,22 +6,26 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
-
+        """Quit command to exit the program"""
         return True
 
     def do_EOF(self, arg):
-        print("")
+        """Exit the program with EOF (Ctrl+D)"""
+        print("")  # print a new line before exiting
         return True
 
     def emptyline(self):
+        """Do nothing on empty line"""
         pass
 
     def help_quit(self):
+        """Prints help message for the quit command"""
         print("Quit command: Exits the program")
-
+    
     def help_EOF(self):
+        """Prints help message for the EOF command"""
         print("EOF command: Exits the program with Ctrl+D")
-
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
+
