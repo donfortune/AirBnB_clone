13de64f4e01a8c9599a46a887a3df9cce472c9d2
donfortune <donfortunet.df@gmail.com>
2024-02-14 03:42:21 -0800

    imported models

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 import json
-from models.user import User  # Import the User class
+from models.user import User
+from models.base_model import BaseModel
+
 
 class FileStorage:
     """FileStorage class
